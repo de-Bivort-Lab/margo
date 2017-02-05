e8fac2ce2c83d56d49f84233dd5b99db2edc6588
Zach Werkhoven <werkhoven@fas.harvard.edu>
2017-02-05 16:03:17 -0500

    version 1.0

--- gui/sub_gui/centerRect.m
+++ gui/sub_gui/centerRect.m
@@ -1,3 +1,3 @@
 function [rect_bounds] = centerRect(centroids,radii)
-    rect_bounds = [centroids(:,1)-radii centroids(:,2)-radii centroids(:,1)+radii centroids(:,2)+radii];
+    rect_bounds = [centroids(:,1)-radii centroids(:,2)-radii radii*2 radii*2];
 end
