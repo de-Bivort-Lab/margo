3ae9c078c7b33f07d21bbd3806e5d7f57f5afb4f
Zach Werkhoven <werkhoven@fas.harvard.edu>
2017-05-18 18:16:36 -0400

    version 1.0

--- helper fcns/blockActivity.m
+++ helper fcns/blockActivity.m
@@ -50,13 +50,14 @@ function idx = filterShortBouts(starts,stops)
     long_bout = bout_length > 15;
     idx = [starts{:}(long_bout) stops{:}(long_bout)];
     
-    if length(idx>1)
+    if size(idx,1)>1
         % ensure that lower index comes first
         [~,i]=min(idx');
         idx(i==2,:) = idx(i==2,[2 1]);
 
         % shift indices to get bouts
         idx = [idx;[idx(1:length(idx)-1,2) idx(2:end,1)]];
+
         bout_length = idx(:,2)-idx(:,1);
         idx(bout_length<30,:)=[];
     end
