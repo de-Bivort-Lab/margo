19506041ff675dc70e375d1c8063b17ac315bf3b
Zach Werkhoven <werkhoven@fas.harvard.edu>
2018-06-20 20:18:42 -0400

    expdata rawfield rawmap defs

--- gui/gui_func/RawDataMap.m
+++ gui/gui_func/RawDataMap.m
@@ -1,51 +1,53 @@
-classdef RawDataField < handle
+classdef RawDataMap < handle
 % class definition for raw data maps container
-
-    properties
-        raw;
-        path;
-        fID;
-        dim;
-        precision;
-    end
     
     properties (Hidden = true)
         map;
     end
     
     methods
-        function obj = RawDataField(varargin)
-            
-            % set default values
-            obj.path = '';
-            obj.precision = 'single';
-            obj.dim = 0;
-            obj.fID = -1;
-            
-            % parse and assign variable inputs
-            for i=1:length(varargin)
-                if ischar(varargin{i})
-                    switch varargin{i}
-                        case 'Path'
-                            i = i+1;
-                            obj.path = varargin{i};
-                        case 'Dim'
-                            i = i+1;
-                            obj.path = varargin{i};
-                        case 'Precision'
-                            i = i+1;
-                            obj.path = varargin{i};
-                        case 'fID'
-                            i = i+1;
-                            obj.path = varargin{i};
-                    end
-                end
-            end
-            
-            
+        function obj = RawDataMap
+                   
         end
         
-        function obj = 
+        function out = subsref(obj,S)
+            
+            if isempty(obj.map)
+                error(['raw data file unattached - run attach(RawDataMap)' ...
+                        ' before indexing raw data']);
+            end
+        end
+        
+        function ans = display(obj)
+            
+            if isempty(obj.map)
+                ans = []
+            else
+                switch ndims(obj.map.Data.raw)
+                    case 1
+                        ans = sprintf(['\traw data file\n\n' ...
+                                '\tsize: \t\t(%ix1)\n'...
+                                '\tprecision: \t%s\n'],...
+                                size(obj.map.Data.raw,1),...
+                                obj.map.Format{1})
+                    case 2
+                        ans = sprintf(['\traw data file\n\n' ...
+                                '\tsize: \t\t(%ix%i)\n'...
+                                '\tprecision: \t%s\n'],...
+                                size(obj.map.Data.raw,1),...
+                                size(obj.map.Data.raw,2),...
+                                obj.map.Format{1})
+                    case 3
+                        ans = sprintf(['\traw data file\n\n' ...
+                                '\tsize: \t\t(%ix%ix%i)\n'...
+                                '\tprecision: \t%s\n'],...
+                                size(obj.map.Data.raw,1),...
+                                size(obj.map.Data.raw,2),...
+                                size(obj.map.Data.raw,3),...
+                                obj.map.Format{1})
+                end
+            end
+        end
         
     end
     
