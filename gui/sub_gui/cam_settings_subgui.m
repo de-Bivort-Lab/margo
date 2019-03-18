aa297fd2f3c5b6af09134aa71eadb945725438a5
Zach Werkhoven <werkhoven@fas.harvard.edu>
2019-03-18 19:58:27 -0400

    allsubguis

--- gui/sub_gui/cam_settings_subgui.m
+++ gui/sub_gui/cam_settings_subgui.m
@@ -74,24 +74,41 @@ end
 names(del) = [];                % remove non-addressable properties
 nColumns = ceil(nControls/12);   % set column number
 
+%% get units of reference controls to populate UI
+
+gui_fig = findall(groot,'Name','margo');
+ref_slider = findall(gui_fig,'Tag','ROI_thresh_slider');
+ref_label = findall(gui_fig,'Tag','ROI_thresh_label');
+ref_edit = findall(gui_fig,'Tag','edit_IR_intensity');
+ref_popup = findall(gui_fig,'Tag','microcontroller_popupmenu');
+slider_w = ref_slider.Position(3);
+edit_h = ref_edit.Position(4);
+edit_w = ref_edit.Position(3)*1.2;
+slider_h = edit_h;
+menu_h = ref_popup.Position(4);
+menu_w = ref_popup.Position(3);
+label_h = ref_label.Position(4);
+w_per_char = ref_label.Position(3)/numel(ref_label.String)*1.4;
+hspacer = ref_edit.Position(3);
+pad = edit_h*2;
+current_height = 0;
+
 %%
 
 %  Create and then hide the UI as it is being constructed.
-font_scale = 6;
-f = figure('Visible','on','Position',...
-    [400,100,(hscale*font_scale + 250)*nColumns,600],'Name','Camera Settings');
+fpos = gui_fig.Position;
+col_w = slider_w + edit_w*2 + hspacer;
+fig_size = [fpos(1:2)+2 col_w*nColumns (edit_h+pad)*12+pad];
+
+f = figure('Visible','on','Units','characters',...
+    'Position',fig_size,'Name','Camera Settings');
 set(f,'MenuBar','none','Toolbar','none','resize','off','NumberTitle','off');
 
 % initialize ui scaling components
-col_w = (hscale*font_scale + 225);
 uival(1) = uicontrol('Style','text','string','','Position',[0 0 0 0]);
 fw = f.Position(3);
 fh = f.Position(4);
-slider_height = 15;
-menu_height = 15;
-label_height = 15;
-spacing = 30;
-current_height = 0;
+
 ct = 0;
 
 
@@ -99,47 +116,66 @@ for i = 1:length(names)
     
     field = info.(names{i});
     if strcmp(field.Constraint,'bounded')
-        current_height = current_height + slider_height + spacing;
+        current_height = current_height + edit_h + pad;
         ct = ct + 1;
 
-        uival(i) = uicontrol('Style','edit','string',num2str(src.(names{i})),'Position',...
-            [hscale*font_scale + 30 + col_w*floor((i-1)/12), (fh-current_height), 60, label_height],...
-            'HorizontalAlignment','left','Callback',@edit_Callback);
+        uival(i) = uicontrol('Style','edit','string',num2str(src.(names{i})),...
+            'Units','characters','Position',...
+            [hspacer + col_w*floor((i-1)/12), ...
+            (fh-current_height), edit_w, edit_h],...
+            'HorizontalAlignment','center','Callback',@edit_Callback);
+        
         uival(i).UserData = i;
         uictl(i) = uicontrol('Style','slider','Min',field.ConstraintValue(1),...
             'Max',field.ConstraintValue(2),'value',src.(names{i}),...
-           'Position',[sum(uival(i).Position([1,3]))+15,(fh-current_height),125,slider_height],...
+           'Units','characters','Position',...
+           [sum(uival(i).Position([1,3]))+hspacer/2,...
+           (fh-current_height), slider_w, slider_h],...
            'Callback',@slider_Callback);
+       
         uictl(i).UserData = i;
-        uilbl(i) = uicontrol('Style','text','string',names{i},'Position',...
-            [10+ col_w*floor((i-1)/12) uictl(i).Position(2) hscale*font_scale label_height],...
+        uilbl(i) = uicontrol('Style','text','string',names{i},...
+            'Units','characters','Position',...
+            [hspacer+col_w*floor((i-1)/12) sum(uictl(i).Position([2 4]))++label_h/4 ...
+            numel(names{i})*w_per_char label_h],...
+            'HorizontalAlignment','left');
+        
+        bound1 = sprintf('%0.2f',field.ConstraintValue(2));
+        uicontrol('Style','text','string',bound1,...
+            'Units','characters','Position',...
+            [sum(uictl(i).Position([1 3]))-numel(bound1)*w_per_char,...
+            sum(uictl(i).Position([2 4]))++label_h/4, numel(bound1)*w_per_char, label_h],...
             'HorizontalAlignment','right');
-        uicontrol('Style','text','string',num2str(round(field.ConstraintValue(2)*100)/100),'Position',...
-            [uictl(i).Position(1)+uictl(i).Position(3)-60 uictl(i).Position(2)-17 60 label_height],...
-            'HorizontalAlignment','right','Units','normalized');
-        uicontrol('Style','text','string',num2str(round(field.ConstraintValue(1)*100)/100),'Position',...
-            [uictl(i).Position(1) uictl(i).Position(2)-17 20 label_height],...
-            'HorizontalAlignment','left','Units','normalized');
-        uictl(i).Units = 'normalized';
-        uilbl(i).Units = 'normalized';
-        uival(i).Units = 'normalized';
+        
+        bound2 = sprintf('%0.2f',field.ConstraintValue(1));
+        uicontrol('Style','text','string',bound2,...
+            'Units','characters','Position',...
+            [uictl(i).Position(1), sum(uictl(i).Position([2 4]))++label_h/4, ...
+            numel(bound2)*w_per_char, label_h],...
+            'HorizontalAlignment','left');
+%         uictl(i).Units = 'normalized';
+%         uilbl(i).Units = 'normalized';
+%         uival(i).Units = 'normalized';
         
 
     end
 
     if strcmp(field.Constraint,'enum')
         ct = ct + 1;
-        current_height = current_height + slider_height + spacing;
+        current_height = current_height + menu_h + pad;
         uictl(i) = uicontrol('Style','popupmenu','string',field.ConstraintValue,...
-                'Position',[hscale*font_scale+col_w*floor((i-1)/12) + 30,fh - current_height,60,slider_height],...
+                'Units','characters','Position',...
+                [col_w*floor((i-1)/12)+hspacer, fh-current_height, menu_w, menu_h],...
                 'Callback',@popupmenu_Callback);
         uictl(i).UserData = i;
-        uilbl(i) = uicontrol('Style','text','string',names{i},'Position',...
-            [10+col_w*floor((i-1)/12) uictl(i).Position(2) hscale*font_scale label_height],...
-            'HorizontalAlignment','right');
+        uilbl(i) = uicontrol('Style','text','string',names{i},...
+            'Units','characters','Position',...
+            [hspacer+col_w*floor((i-1)/12), sum(uictl(i).Position([2 4]))++label_h/4, ...
+            numel(names{i})*w_per_char, label_h],...
+            'HorizontalAlignment','left');
         
-        uictl(i).Units = 'normalized';
-        uilbl(i).Units = 'normalized';
+%         uictl(i).Units = 'normalized';
+%         uilbl(i).Units = 'normalized';
         
         % find current value from src
         str_list = get(uictl(i),'string');
@@ -181,7 +217,7 @@ function slider_Callback(src,event)
     vals= data.uival;
     
     % update coupled UI component
-    set(vals(src.UserData),'string',num2str(get(src,'value')));
+    set(vals(src.UserData),'string',sprintf('%0.2f',src.Value));
     
     % update camera source and settings
     data.expmt.hardware.cam.settings.(names{src.UserData}) = get(src,'value');
@@ -212,7 +248,7 @@ function edit_Callback(src,event)
     ctls = data.uictl;
     
     % update camera source and settings with current value of src.string
-    val = str2num(get(src,'string'));
+    val = str2double(get(src,'string'));
     info = propinfo(data.cam_src);
     if isfield(info,(names{src.UserData})) && ...
             isfield(info.(names{src.UserData}),'Constraint')
@@ -227,7 +263,7 @@ function edit_Callback(src,event)
     set(ctls(src.UserData),'value',val);   
     data.expmt.hardware.cam.settings.(names{src.UserData}) = val;  
     data.cam_src.(names{src.UserData}) = val;
-    src.String = num2str(val);
+    src.String = sprintf('%0.2f',val);
     set(pf,'UserData',data); 
     
 end
