c349a3f8f43bd0854d5a288e7b7ce1acf998c563
Zach Werkhoven <zach.werkhoven@fpsinc.com>
2023-04-07 21:39:48 -0400

    Fixed static method references and implicit dimension assignment in nanFilterredStatistics

--- utilities/nan-filtered-statistics/nanFilteredMax.m
+++
@@ -1,9 +0,0 @@
-function out = nanFilteredMax(x, varargin)
-%NANFILTEREDMEAN Summary of this function goes here
-%   Detailed explanation goes here
-
-FUNCTION_ID = "NANFILTEREDMAX";
-out = NanFilteredStatistic("max", FUNCTION_ID, x, varargin{:}).apply();
-
-end
-
