00ff5a0c59b24b5a56f32162adab7f3ee8070ca3
Zach Werkhoven <zach.werkhoven@fpsinc.com>
2023-04-13 15:43:32 -0400

    Fixed an issue where MATLAB inconsistently serialized serial devices across different releases

--- hardware/serial/ComHardware.m
+++ hardware/serial/ComHardware.m
@@ -28,7 +28,7 @@ classdef ComHardware < handle
             this.closeOpenConnections();
             this.updatePortsList();
             
-            for i = 1:size(this.ports, 2)
+            for i = 1:size(this.ports, 1)
                 this.devices(i) = SerialDevice(this.ports{i});
                 isSuccessful = ComHardware.handshakeDevice(this.devices(i));
                 if isSuccessful
@@ -48,6 +48,7 @@ classdef ComHardware < handle
 
         function updatePortsList(this)
             this.ports = SerialDevice.getAvailablePorts();
+            this.ports = this.ports(:);
         end
 
         function writeLightPanel(this, lightPanel, value)
